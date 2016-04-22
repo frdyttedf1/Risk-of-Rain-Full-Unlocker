1fa55fb6c228d73df4c62b7647bc0a8ea1b23b48
frdyttedf1 <asherbordelon@icloud.com>
2016-04-22 10:27:38 -0500

    Added files via upload

---
+++ RiskOfRainUnlocker.bat
@@ -0,0 +1,173 @@
+@echo off
+title Risk of Rain Unlocker
+echo Risk of Rain Unlocker
+echo -- Copyright 2016 Asher Bordelon --
+echo.
+echo This program will change your save file to unlock everything in your game.
+echo.
+echo WARNING! This WILL erase all of your stats.
+echo.
+echo Please make sure this file is inside of your Risk of Rain game folder before selecting 'Yes'
+echo.
+echo Would you like to unlock everything in Risk of Rain? (Y/N)
+set /p yn=
+if %yn%==n exit
+if %yn%==N exit
+echo Alright, here we go!
+echo [Achievement] > save.ini
+echo achievement_number_unlocked=55 >> save.ini
+echo achievement0=2 >> save.ini
+echo achievement1=2 >> save.ini
+echo achievement2=2 >> save.ini
+echo achievement3=2 >> save.ini
+echo achievement4=2 >> save.ini
+echo achievement5=2 >> save.ini
+echo achievement6=2 >> save.ini
+echo achievement7=2 >> save.ini
+echo achievement8=2 >> save.ini
+echo achievement9=2 >> save.ini
+echo achievement10=2 >> save.ini
+echo achievement11=2 >> save.ini
+echo achievement12=2 >> save.ini
+echo achievement13=2 >> save.ini
+echo achievement14=2 >> save.ini
+echo achievement15=2 >> save.ini
+echo achievement16=2 >> save.ini
+echo achievement17=2 >> save.ini
+echo achievement18=2 >> save.ini
+echo achievement19=2 >> save.ini
+echo achievement20=2 >> save.ini
+echo achievement21=2 >> save.ini
+echo achievement22=2 >> save.ini
+echo achievement23=2 >> save.ini
+echo achievement24=2 >> save.ini
+echo achievement25=2 >> save.ini
+echo achievement26=2 >> save.ini
+echo achievement27=2 >> save.ini
+echo achievement28=2 >> save.ini
+echo achievement29=2 >> save.ini
+echo achievement30=2 >> save.ini
+echo achievement31=2 >> save.ini
+echo achievement32=2 >> save.ini
+echo achievement33=2 >> save.ini
+echo achievement34=2 >> save.ini
+echo achievement35=2 >> save.ini
+echo achievement36=2 >> save.ini
+echo achievement37=2 >> save.ini
+echo achievement38=2 >> save.ini
+echo achievement39=2 >> save.ini
+echo achievement40=2 >> save.ini
+echo achievement41=2 >> save.ini
+echo achievement42=2 >> save.ini
+echo achievement43=2 >> save.ini
+echo achievement44=2 >> save.ini
+echo achievement45=2 >> save.ini
+echo achievement46=2 >> save.ini
+echo achievement47=2 >> save.ini
+echo achievement48=2 >> save.ini
+echo achievement49=2 >> save.ini
+echo achievement50=2 >> save.ini
+echo achievement51=2 >> save.ini
+echo achievement52=2 >> save.ini
+echo achievement53=2 >> save.ini
+echo achievement54=2 >> save.ini
+echo achievement_progress14=1490 >> save.ini
+echo achievement_progress15=0 >> save.ini
+echo achievement_progress16=0 >> save.ini
+echo achievement_progress20=4 >> save.ini
+echo achievement_progress23=6 >> save.ini
+echo achievement_progress33=0 >> save.ini
+echo achievement_progress35=18 >> save.ini
+echo achievement_progress41=0 >> save.ini
+echo achievement_progress47=0 >> save.ini
+echo achievement_progress48=0 >> save.ini
+echo achievement_progress50=0 >> save.ini
+echo achievement_progress53=75 >> save.ini
+echo [Record] >> save.ini
+echo achievement_number_unlocked=8 >> save.ini
+echo artifact0=1 >> save.ini
+echo artifact1=1 >> save.ini
+echo artifact2=1 >> save.ini
+echo artifact3=1 >> save.ini
+echo artifact4=1 >> save.ini
+echo artifact5=1 >> save.ini
+echo artifact6=1 >> save.ini
+echo artifact7=1 >> save.ini
+echo artifact8=1 >> save.ini
+echo artifact9=1 >> save.ini
+echo record_level=0 >> save.ini
+echo record_time=0 >> save.ini
+echo record_kills=0 >> save.ini
+echo record_bosses=0 >> save.ini
+echo record_item=0 >> save.ini
+echo record_gold=0 >> save.ini
+echo record_points=0 >> save.ini
+echo record_kills_points=0 >> save.ini
+echo record_bosses_points=0 >> save.ini
+echo record_char[12]=0 >> save.ini
+echo item17=1 >> save.ini
+echo item_number_unlocked=30 >> save.ini
+echo item44=1 >> save.ini
+echo arch_enemy_list= >> save.ini
+echo general_stats[0]=0 >> save.ini
+echo general_stats[1]=0 >> save.ini
+echo general_stats[2]=0 >> save.ini
+echo general_stats[3]=0 >> save.ini
+echo general_stats[4]=0 >> save.ini
+echo general_stats[5]=0 >> save.ini
+echo trophy_stats[0]=0 >> save.ini
+echo trophy_stats[1]=0 >> save.ini
+echo trophy_stats[2]=0 >> save.ini
+echo trophy_stats[3]=0 >> save.ini
+echo trophy_stats[4]=0 >> save.ini
+echo trophy_stats[5]=0 >> save.ini
+echo trophy_stats[6]=0 >> save.ini
+echo trophy_stats[7]=0 >> save.ini
+echo trophy_stats[8]=0 >> save.ini
+echo trophy_stats[9]=0 >> save.ini
+echo trophy_stats[10]=0 >> save.ini
+echo trophy_stats[11]=0 >> save.ini
+echo trophy_stats[12]=0 >> save.ini
+echo item11=1 >> save.ini
+echo item12=1 >> save.ini
+echo item24=1 >> save.ini
+echo item37=1 >> save.ini
+echo mons0=1 >> save.ini
+echo mons_number_unlocked=5 >> save.ini
+echo record_chest=0 >> save.ini
+echo item3=1 >> save.ini
+echo item19=1 >> save.ini
+echo item20=1 >> save.ini
+echo item48=1 >> save.ini
+echo item101=1 >> save.ini
+echo record_char[4]=2 >> save.ini
+echo item4=1 >> save.ini
+echo item5=1 >> save.ini
+echo item18=1 >> save.ini
+echo item21=1 >> save.ini
+echo item23=1 >> save.ini
+echo item33=1 >> save.ini
+echo item47=1 >> save.ini
+echo item72=1 >> save.ini
+echo item75=1 >> save.ini
+echo item6=1 >> save.ini
+echo item10=1 >> save.ini
+echo item32=1 >> save.ini
+echo item7=1 >> save.ini
+echo item73=1 >> save.ini
+echo mons1=1 >> save.ini
+echo record_char[11]=1 >> save.ini
+echo item25=1 >> save.ini
+echo item27=1 >> save.ini
+echo item39=1 >> save.ini
+echo item100=1 >> save.ini
+echo mons5=1 >> save.ini
+echo mons9=1 >> save.ini
+echo item9=1 >> save.ini
+echo item14=1 >> save.ini
+cls
+echo All done! Enjoy your game :)
+echo -- Copyright 2016 Asher Bordelon --
+echo Written in batch
+pause
+exit
